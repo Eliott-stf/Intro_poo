42235a50f9355d87de0b38073fde3b69316b5447
ES_PREPA_DEV <setifeliott@gmail.com>
2025-11-19 12:12:18 +0100

    Classe Pen méthode & propriétée fonctionnelles

--- www/public/index.php
+++ www/public/index.php
@@ -1,4 +1,22 @@
 <?php
 
-// Supprimer phpinfo() et commencer à coder !
-phpinfo();
+const DS = DIRECTORY_SEPARATOR;
+// Import des fichiers sources des classes 
+require_once dirname(__FILE__, 2) . DS . 'src' . DS . 'App' . DS . 'Pen.php';
+
+// On déclare que l'on veut utiliser dans ce fichier la classe Pen
+//App\Pen est le "nom complètement qualifié" (fully qualified name) de la classe  
+use App\Pen;
+
+// Instantiation de Pen: On crée un objet sur le modèle de la classe Pen 
+$bic_pas_cher = new Pen();
+//modification d'une propriété publique
+$bic_pas_cher->color = 'rgba(136, 20, 0, 1)';
+
+$bic_bleu = new Pen;
+$bic_bleu->color = '#00c';
+
+//Appel d'une méthode publique 
+$bic_pas_cher->write('Bonjour');
+$bic_bleu->write('oh mais c\'est bleu!');
+$bic_pas_cher->write('Je prefere celui-ci finalement');
---
+++ www/src/App/Pen.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Classe Pen
+ */
+
+// Déclaration du namespace pour cette classe 
+namespace App;
+
+//Déclaration de la classe 
+class Pen 
+{
+    // Propriété: variable qui est membre d'une classe
+    // C'est une caractèristique de l'objet crée
+    public string $color;
+
+    //Méthode: fonction qui est membre d'une classe
+    // C'est une fonctionnalité de l'objet crée
+    public function write( string $message ): void
+    {
+        // $this contient l'instance actuelle (sur laquelle la méthode a été appelée) 
+        /* $result = '<span style ="color:' . $this->color . '">'. $message . '</span>'; */
+        //Fonctions de formatage de chaine:
+        // - printf(): formate et echo le résultat
+        // - sprintf(): formate et retourne le résultat 
+
+/*         $result = sprintf(
+            '<span style ="color:%s">%s</span>',
+            $this->color,
+             $message
+        );
+ */
+        // echo incrémente le corps de la réponse HTTP avec la chaine donnée 
+        /* echo $result; */
+        
+        printf(
+            '<span style ="color:%s">%s</span>',
+            $this->color,
+             $message
+        );
+
+    }
+}
